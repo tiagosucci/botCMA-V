a36ac3cb386bed177399d69002d703e0b0c03798
Tiago Succi <34147114+tiagosucci@users.noreply.github.com>
2024-07-22 08:37:24 -0300

    start

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+const token = 'YOUR_DISCORD_BOT_TOKEN'; // Substitua pelo seu token
+
+client.once('ready', () => {
+    console.log('Bot está online!');
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '!ping') {
+        message.channel.send('Pong!');
+    }
+});
+
+client.login(token);
